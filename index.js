5ee5b979e058975d5c521fd1279fecaf93d35f2a
Ryan Lee <ryanm.lee11@gmail.com>
2020-07-30 23:03:58 +0000

    Update with Date convert function

--- index.js
+++ index.js
@@ -26,3 +26,34 @@ try {
 } catch(err) {
   console.log(err);
 }
+
+//Challenge 2 - Remove Property
+// Implement the removeProperty function which takes an object and property name, and does the following:
+
+// If the object obj has a property prop, the function removes the property from the object and returns true; in all other cases it returns false.
+
+//Solution 1 - hasOwnProperty and if Statement
+function removeProperty(obj, prop) {
+  if (obj.hasOwnProperty(prop)) {
+    delete obj[prop];
+    return true;
+  }
+  return false;
+}
+
+//Solution 2 - Object.keys and ternary operator
+function removeProperty(obj, prop) {
+  return (Object.keys(obj).indexOf(prop) > -1) ? delete obj[prop] : false;
+}
+
+//Challenge 3 - Date
+// Write a function that converts user entered date formatted as M/D/YYYY to a format required by an API (YYYYMMDD). The parameter "userDate" and the return value are strings.
+
+// For example, it should convert user entered date "12/31/2014" to "20141231" suitable for the API.
+
+function formatDate(userDate) {
+  const x = userDate.split('/');
+  return `${x[2]}${x[1]}${x[0]}`;
+}
+
+console.log(formatDate("12/31/2014"));
