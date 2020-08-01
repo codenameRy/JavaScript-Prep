9a5da095e53cf38f9c726398dc604b28ee8ee6db
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-01 02:58:55 +0000

    Update

--- index.js
+++ index.js
@@ -73,3 +73,30 @@ function formatDate2(userDate) {
 }
 
 console.log(formatDate2("10/2/2020"));
+
+//Challenge - Image Gallery
+
+function setup() {
+  var items = document.getElementsByClassName("remove");
+  for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', function removeParentDiv() {
+      this.parentNode.remove();
+    });
+  }
+}
+
+// Example case. 
+document.body.innerHTML = `
+<div class="image">
+  <img src="https://goo.gl/kjzfbE" alt="First">
+  <button class="remove">X</button>
+</div>
+<div class="image">
+  <img src="https://goo.gl/d2JncW" alt="Second">
+  <button class="remove">X</button>
+</div>`;
+
+setup();
+
+document.getElementsByClassName("remove")[0].click();
+console.log(document.body.innerHTML);
